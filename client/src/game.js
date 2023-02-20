a7d47a61a1c2e706ae51be84f7ab1687083ac146
Shafali Gupta <shafaligupta31@gmail.com>
2023-02-20 15:55:02 -0500

    added a starter image into phaser

--- client/src/game.js
+++ client/src/game.js
@@ -1,32 +1,79 @@
+// var config = {
+//     type: Phaser.AUTO,
+//     parent: 'phaser-example',
+//     width: 800,
+//     height: 600,
+//     physics: {
+//       default: 'arcade',
+//       arcade: {
+//         debug: false,
+//         gravity: { y: 0 }
+//       }
+//     },
+//     scene: {
+//       preload: preload,
+//       create: create,
+//       update: update
+//     } 
+//   };
+
+// var game = new Phaser.Game(config);
+  
+// function preload() {
+
+// }
+  
+// function create() {
+//     this.socket = io();
+// }
+  
+// function update() {
+
+// }
 var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    physics: {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
       default: 'arcade',
       arcade: {
-        debug: false,
-        gravity: { y: 0 }
+          gravity: { y: 200 }
       }
-    },
-    scene: {
+  },
+  scene: {
       preload: preload,
-      create: create,
-      update: update
-    } 
-  };
+      create: create
+  }
+};
 
 var game = new Phaser.Game(config);
-  
-function preload() {
 
-}
-  
-function create() {
-    this.socket = io();
-}
-  
-function update() {
+function preload ()
+{
+  this.load.setBaseURL('http://labs.phaser.io');
 
+  this.load.image('sky', 'assets/skies/space3.png');
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+  this.load.image('red', 'assets/particles/red.png');
+}
+
+function create ()
+{
+  this.add.image(400, 300, 'sky');
+
+  var particles = this.add.particles('red');
+
+  var emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+  });
+
+  var logo = this.physics.add.image(400, 100, 'logo');
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
 }
