cd5e200f56d0160a12da9e1a6766f6ee8475440c
christinabuencamino <c.buencamino123@gmail.com>
2023-02-26 13:11:06 -0500

    Static home page, trying to get buttons to work

--- client/src/game.js
+++ client/src/game.js
@@ -1,3 +1,83 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#FCFBF4',
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 200 }
+      }
+  },
+  scene: {
+      preload: preload,
+      create: create,
+  }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+  // this.load.html("how_to_play", "client/assets/how_to_play.html");
+  this.load.setBaseURL('http://labs.phaser.io');
+  this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'); // google font API
+}
+
+var text = null;
+
+function create ()
+{
+  this.socket = io();
+  var add = this.add;
+
+  WebFont.load({
+    google: {
+        families: [ 'Chela One' ]
+    },
+    active: function ()
+      {
+        // title
+        add.text(125, 110, 'Cat', { 
+          fontFamily: 'Chela One', 
+          fontSize: 100, 
+          color: '#F8F0C6' 
+        });
+        add.text(250, 110, 'Burglars', { 
+          fontFamily: 'Chela One', 
+          fontSize: 100, 
+          color: '#C1A87D' 
+        });
+        add.text(565, 110, '.io', { 
+          fontFamily: 'Chela One', 
+          fontSize: 100, 
+          color: '#EFC482' 
+        });
+
+        // buttons
+        const playButton = add.text(400, 325, 'Start', { 
+          fontFamily: 'Chela One', 
+          fontSize: 60, 
+          color: '#FFFBF4', 
+          backgroundColor: "#C1A87D"
+        })
+          .setOrigin(0.5)
+          .setPadding(10,10,10,10);
+        const howToPlayButton = add.text(400, 440, 'How To Play', { 
+          fontFamily: 'Chela One', 
+          fontSize: 60, 
+          color: '#FFFBF4', 
+          backgroundColor: "#C1A87D"
+        })
+          .setOrigin(0.5)
+          .setPadding(10,10,10,10);
+
+        playButton.setInteractive();
+        howToPlayButton.setInteractive();
+      }
+  });
+}
+
 // var config = {
 //     type: Phaser.AUTO,
 //     parent: 'phaser-example',
@@ -30,53 +110,3 @@
 // function update() {
 
 // }
-var config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  backgroundColor: '#FCFBF4',
-  physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 200 }
-      }
-  },
-  scene: {
-      preload: preload,
-      create: create,
-  }
-};
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-  this.load.setBaseURL('http://labs.phaser.io');
-  this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'); // google font API
-}
-
-var text = null;
-
-function create ()
-{
-  this.socket = io();
-  var add = this.add;
-
-  WebFont.load({
-    google: {
-        families: [ 'Chela One' ]
-    },
-    active: function ()
-    {
-      add.text(125, 110, 'Cat', { fontFamily: 'Chela One', fontSize: 100, color: '#F8F0C6' });
-      add.text(250, 110, 'Burglars', { fontFamily: 'Chela One', fontSize: 100, color: '#C1A87D' });
-      add.text(565, 110, '.io', { fontFamily: 'Chela One', fontSize: 100, color: '#EFC482' });
-      const playButton = add.text(400, 325, 'Start', { fontFamily: 'Chela One', fontSize: 60, color: '#D1C0A8' }).setOrigin(0.5);
-      // playButton.backgroundColor('#D1C0A8');
-      const howToPlayButton = add.text(400, 425, 'How To Play', { fontFamily: 'Chela One', fontSize: 60, color: '#D1C0A8'}).setOrigin(0.5);
-
-      playButton.setInteractive();
-      howToPlayButton.setInteractive();
-    }
-  });
-}
---
+++ client/src/scenes/LobbyScene.js
@@ -0,0 +1,14 @@
+import Phaser from "phaser";
+
+export default class LobbyScene extends Phaser.Scene {
+    constructor() {
+        super("LobbyScene");
+        this.state = {};
+    }
+
+    preload() {}
+
+    create() {}
+
+    update() {}
+}
