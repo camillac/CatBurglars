04e501e863b8283cd460467bb20ed9bfe164971e
christinabuencamino <c.buencamino123@gmail.com>
2023-02-25 22:44:36 -0500

    Basic home page, nothing working yet

--- client/src/game.js
+++ client/src/game.js
@@ -34,6 +34,7 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  backgroundColor: '#FCFBF4',
   physics: {
       default: 'arcade',
       arcade: {
@@ -42,7 +43,7 @@ var config = {
   },
   scene: {
       preload: preload,
-      create: create
+      create: create,
   }
 };
 
@@ -51,30 +52,31 @@ var game = new Phaser.Game(config);
 function preload ()
 {
   this.load.setBaseURL('http://labs.phaser.io');
-
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
+  this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'); // google font API
 }
+
+var text = null;
 
 function create ()
 {
   this.socket = io();
-  this.add.image(400, 300, 'sky');
+  var add = this.add;
 
-  var particles = this.add.particles('red');
+  WebFont.load({
+    google: {
+        families: [ 'Chela One' ]
+    },
+    active: function ()
+    {
+      add.text(125, 110, 'Cat', { fontFamily: 'Chela One', fontSize: 100, color: '#F8F0C6' });
+      add.text(250, 110, 'Burglars', { fontFamily: 'Chela One', fontSize: 100, color: '#C1A87D' });
+      add.text(565, 110, '.io', { fontFamily: 'Chela One', fontSize: 100, color: '#EFC482' });
+      const playButton = add.text(400, 325, 'Start', { fontFamily: 'Chela One', fontSize: 60, color: '#D1C0A8' }).setOrigin(0.5);
+      // playButton.backgroundColor('#D1C0A8');
+      const howToPlayButton = add.text(400, 425, 'How To Play', { fontFamily: 'Chela One', fontSize: 60, color: '#D1C0A8'}).setOrigin(0.5);
 
-  var emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: 'ADD'
+      playButton.setInteractive();
+      howToPlayButton.setInteractive();
+    }
   });
-
-  var logo = this.physics.add.image(400, 100, 'logo');
-
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
-
-  emitter.startFollow(logo);
 }
