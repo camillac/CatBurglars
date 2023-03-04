88b905942e3be727ddebdee5c55bb22236dc34bf
hamidouwaraballo <ballowara@gmail.com>
2023-03-04 18:03:05 -0500

    players oject

--- server/src/sockets.js
+++ server/src/sockets.js
@@ -1,5 +1,24 @@
 module.exports = (io) => {
+    let players = {}
+
     io.on("connection", (socket) => {
-      console.log(`A socket connection to the server has been made: ${socket.id}`)
+      console.log(`A socket connection to the server has been made: ${socket.id}`);
+      if(players.l < 4) {
+          players[socket.id] = {
+          playerId: socket.id,
+          room:null,
+          }
+      } else {
+          socket.on("disconnected", () => {
+          delete players[socket.id];
+          io.emit('playerDisconnected', socket.id);
+      });
+      }
+
+
+
+
+      console.log(players)
+
     });
 }
