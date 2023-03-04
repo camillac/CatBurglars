d93ffd9fd7e27fcf7c7163c155e79dd9d0b4028b
BorisBil <borisbilogur@gmail.com>
2023-03-04 13:26:07 -0500

    Connection Counter
    Added counter for connections to the server as well as a disconnect message

--- server/src/sockets.js
+++ server/src/sockets.js
@@ -1,5 +1,13 @@
 module.exports = (io) => {
-    io.on("connection", (socket) => {
-      console.log(`A socket connection to the server has been made: ${socket.id}`)
+  var connectCounter = 0;
+  io.on("connection", (socket) => {
+    console.log(`A socket connection to the server has been made: ${socket.id}`)
+    connectCounter++;
+    console.log(connectCounter);
+    socket.on("disconnect", () => {
+      console.log(`A socket has disconnected`);
+      connectCounter--;
+      console.log(connectCounter);
     });
+  });
 }
