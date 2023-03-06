f360b17868569e28235105224468c9a479fc97c2
hamidouwaraballo <ballowara@gmail.com>
2023-03-06 07:25:57 -0500

    players object

--- server/src/sockets.js
+++ server/src/sockets.js
@@ -3,22 +3,23 @@ module.exports = (io) => {
 
     io.on("connection", (socket) => {
       console.log(`A socket connection to the server has been made: ${socket.id}`);
-      if(players.l < 4) {
+
+      if(Object.keys(players).length < 4) {
           players[socket.id] = {
           playerId: socket.id,
           room:null,
-          }
-      } else {
-          socket.on("disconnected", () => {
-          delete players[socket.id];
-          io.emit('playerDisconnected', socket.id);
-      });
       }
 
+      // Send the players object to the new player.
+      socket.emit('currentPlayers', players);
+      } else {
+          socket.on("disconnect", () => {
+          delete players[socket.id];
+          // io.emit('playerDisconnected', socket.id);
+          console.log(`Player ${socket.id} disconnected`);
+          });
+      }
 
-
-
-      console.log(players)
-
+      console.log(players);
     });
 }
