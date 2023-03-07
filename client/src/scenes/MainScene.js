94ae1d5adf3d0c060f18ea77cadc6d8569c83d4f
christinabuencamino <c.buencamino123@gmail.com>
2023-03-07 12:18:24 -0500

    Fixing merge conflicts, worked on ui

--- client/src/scenes/MainScene.js
+++ client/src/scenes/MainScene.js
@@ -5,9 +5,8 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.setBaseURL('http://labs.phaser.io');
         this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
-
+        this.load.image('background', 'client/assets/backgrounds/blob-scene-haikei (6).png');
     }
 
     create() {
@@ -16,28 +15,43 @@ export default class MainScene extends Phaser.Scene {
         const scene = this;
         scene.roomKey = '';
 
+        const background = this.add.image(400, 300, "background")
+        background.setScale(2.0)
+
         WebFont.load({
             google: {
                 families: ['Chela One']
             },
             active: function () {
+                
+
                 // title
                 add.text(125, 110, 'Cat', {
                     fontFamily: 'Chela One',
                     fontSize: 100,
-                    color: '#F8F0C6'
+                    color: '#F8F0C6',
+                    fontStyle: 'normal',
+                    stroke: '#000000',
+                    strokeThickness: 12
                 });
+                
 
                 add.text(250, 110, 'Burglars', {
                     fontFamily: 'Chela One',
                     fontSize: 100,
-                    color: '#C1A87D'
+                    color: '#C1A87D',
+			fontStyle: 'normal',
+			stroke: '#000000',
+			strokeThickness: 12
                 });
 
                 add.text(565, 110, '.io', {
                     fontFamily: 'Chela One',
                     fontSize: 100,
-                    color: '#EFC482'
+                    color: '#EEBA6B',
+			fontStyle: 'normal',
+			stroke: '#000000',
+			strokeThickness: 12
                 });
 
                 // buttons
@@ -45,7 +59,10 @@ export default class MainScene extends Phaser.Scene {
                     fontFamily: 'Chela One',
                     fontSize: 60,
                     color: '#FFFBF4',
-                    backgroundColor: "#C1A87D"
+
+			fontStyle: 'normal',
+			stroke: '#000000',
+			strokeThickness: 12
                 })
                     .setOrigin(0.5)
                     .setPadding(10, 10, 10, 10);
@@ -56,7 +73,10 @@ export default class MainScene extends Phaser.Scene {
                     fontFamily: 'Chela One',
                     fontSize: 60,
                     color: '#FFFBF4',
-                    backgroundColor: "#C1A87D"
+
+			fontStyle: 'normal',
+			stroke: '#000000',
+			strokeThickness: 12
                 })
                     .setOrigin(0.5)
                     .setPadding(10, 10, 10, 10);
@@ -64,9 +84,31 @@ export default class MainScene extends Phaser.Scene {
                 playButton.setInteractive();
                 howToPlayButton.setInteractive();
 
-                // how to pop up
+                // how to play button events
+                howToPlayButton.on('pointerover', () => {
+                    howToPlayButton.setStyle({
+                        color: '#FFEBB9'
+                    })
+                });
+                howToPlayButton.on('pointerout', () => {
+                    howToPlayButton.setStyle({
+                        color: '#FFFBF4'
+                    })
+                });
                 howToPlayButton.on('pointerup', () => {
-                    scene.scene.launch("HowtoPlayScene", { ...scene.state, socket: scene.socket });
+                    scene.scene.start("HowtoPlayScene", { ...scene.state, socket: scene.socket });
+                });
+
+                // play button events
+                playButton.on('pointerover', () => {
+                    playButton.setStyle({
+                        color: '#FFEBB9'
+                    })
+                });
+                playButton.on('pointerout', () => {
+                    playButton.setStyle({
+                        color: '#FFFBF4'
+                    })
                 });
                 playButton.on('pointerup', () => {
                     scene.socket.emit("getRoomCode");
