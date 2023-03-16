aae266c5a7404d1ae2939342c7422d1d293c66d3
hamidouwaraballo <ballowara@gmail.com>
2023-03-16 19:33:33 -0400

    Adding setting icon.

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import MainScene from "./scenes/MainScene.js";
 import HowtoPlayScene from "./scenes/HowtoPlayScene.js";
 import LobbyScene from "./scenes/LobbyScene.js";
 import PlayScene from "./scenes/PlayScene.js";
-// import FirstTask from "./scenes/FirstTask";
+import FirstTask from "./scenes/FirstTask.js";
 
 // Insert all of the scenes into the game
 class Game extends Phaser.Game {
@@ -14,7 +14,7 @@ class Game extends Phaser.Game {
         this.scene.add("HowtoPlayScene", HowtoPlayScene);
         this.scene.add("LobbyScene", LobbyScene);
         this.scene.add("PlayScene", PlayScene);
-        // this.scene.add("FirstTask", FirstTask);
+        this.scene.add("FirstTask", FirstTask);
 
         this.scene.start("MainScene");
     }
--- client/src/scenes/FirstTask.js
+++ client/src/scenes/FirstTask.js
@@ -12,20 +12,28 @@ export default class FirstTask extends Phaser.Scene {
             "background",
             "client/assets/backgrounds/blob-scene-haikei (6).png"
         );
+        this.load.image(
+            "settings",
+            "client/assets/backgrounds/settings_icon"
+        );
     }
 
     create() {
         const scene = this;
+        // Setting up background for the game
         const background = this.add.image(400, 300, "background");
         background.setScale(2.0);
 
+        // Left side menu dimensions
         const width = this.game.config.width;
         const height = this.game.config.height;
 
         const menu = this.add.graphics();
-        menu.fillStyle(0x808080, 1);
-        menu.fillRect(0, 0, width/3, height);
+        menu.fillStyle(0xBBC2CC, 1);
+        menu.fillRect(0, 0, width/4, height);
+
+        // Display players
     }
 
-    upload() {}
+    update() {}
 }
