308cb7877c24e9fc9ec431d155d566918f96b6a2
hamidouwaraballo <ballowara@gmail.com>
2023-03-16 20:06:34 -0400

    Added settings button working. Next working on the pop up that opens after the button is clicked

--- client/src/scenes/FirstTask.js
+++ client/src/scenes/FirstTask.js
@@ -14,7 +14,7 @@ export default class FirstTask extends Phaser.Scene {
         );
         this.load.image(
             "settings",
-            "client/assets/backgrounds/settings_icon"
+            "client/assets/backgrounds/settings_icon.png"
         );
     }
 
@@ -32,8 +32,31 @@ export default class FirstTask extends Phaser.Scene {
         menu.fillStyle(0xBBC2CC, 1);
         menu.fillRect(0, 0, width/4, height);
 
-        // Display players
+        // Add the setting wheel button
+        const settingsBtn = this.add.image(100, 530, 'settings').setInteractive();
+        settingsBtn.on('pointerup', () => {
+            // Open the settings popup
+            this.showSettingsPopup();
+        });
     }
 
+
+
     update() {}
+
+    showSettingsPopup() {
+        // Create and display the settings popup
+        const popup = this.add.container(this.game.config.width / 2, this.game.config.height / 2);
+        const background = this.add.graphics();
+        background.fillStyle(0xffffff, 1);
+        background.fillRect(-150, -150, 300, 300);
+        const closeButton = this.add.text(130, -130, 'X', {fontSize: '24px'}).setInteractive();
+        closeButton.on('pointerup', () => {
+            // Close the popup
+            popup.destroy();
+        });
+        popup.add(background);
+        popup.add(closeButton);
+        // Pop Up Seeings
+    }
 }
