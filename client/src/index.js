d0feddfd2909ef4127cc563e70e43b53b57d9f1c
Shafali Gupta <shafaligupta31@gmail.com>
2023-04-16 20:21:35 -0400

    fixed duplicate

--- client/src/index.js
+++ client/src/index.js
@@ -26,8 +26,6 @@ class Game extends Phaser.Game {
         this.scene.add("CreateLobbyScene", CreateLobbyScene);
         this.scene.add("JoinLobbyScene", JoinLobbyScene);
         this.scene.add("WinningScene", WinningScene);
-        this.scene.add("IntroductionScene", IntroductionScene);
-        this.scene.add("FirstTask_Instruction", FirstTask_Instruction);
 
         this.scene.start("MainScene");
         // this.events.off("hidden", this.onHidden, this, true);
